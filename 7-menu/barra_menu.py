d4bfe52d00696675fef10d1bfd1d574b674bd260
Daniel <djaguiar13@gmai.com>
2025-01-07 09:25:12 -0500

    As

--- 7-menu/barra_menu.py
+++ 7-menu/barra_menu.py
@@ -8,13 +8,12 @@ ventana.config(menu=barra_menu)
 archivo_menu = tk.Menu(barra_menu)
 barra_menu.add_cascade(label="Archivo", menu=archivo_menu)
 
+def abrir_archivo():
+   print("Archivo Abierto")
 
-#def abrir_archivo():
-#    print("Archivo Abierto")
-
-#menu.add_command(label="Abrir", command=abrir_archivo)
+archivo_menu.add_command(label='Nuevo')
+archivo_menu.add_command(label="Abrir", command=abrir_archivo)
+archivo_menu.add_separator() # Separa
+archivo_menu.add_command(label="Salir")
 
 ventana.mainloop()
-
-#menu.add_command(label="Abrir")
-#menu.add_command(label="Guardar")
