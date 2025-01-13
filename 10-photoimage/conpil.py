f3a4419afeb5b73e161b2b4f873490ad6774a8ad
Daniel <djaguiar13@gmai.com>
2025-01-13 10:29:24 -0500

    E11

--- 10-photoimage/conpil.py
+++ 10-photoimage/conpil.py
@@ -6,7 +6,8 @@ etiqueta = tk.Label(ventana)
 
 image_pil = Image.open("../static/image/wedding-4780256_1280.jpg")
 imagen_redimensionada = image_pil.resize((50,50))
-image_tk = ImageTk.PhotoImage(imagen_redimensionada)
+imagen_rotada = imagen_redimensionada.rotate(45)
+image_tk = ImageTk.PhotoImage(imagen_rotada)
 
 boton = tk.Button(ventana, image=image_tk)
 boton.pack()
